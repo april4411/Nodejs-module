c1cefc0e8b640750198c123f7b20dc4004879d8a
april4411 <lovelyapril4@naver.com>
2022-08-09 16:57:16 +0900

    Start

---
+++ index.js
@@ -0,0 +1,7 @@
+const { checkNumberOddOrEven, checkStringOddOrEven } = require('./func.js')
+
+// 숫자 10는 짝수인가 홀수인가
+console.log(checkNumberOddOrEven(10))
+
+//문자 'HELLO'는 짝수인가 홀수인가
+console.log(checkStringOddOrEven('HELLO'))
