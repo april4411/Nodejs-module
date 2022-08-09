01b4280c89f810f865b3ef5ad7576addaf46dc29
april4411 <110403930+april4411@users.noreply.github.com>
2022-08-09 17:06:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ console.log(checkNumberOddOrEven(10))
 
 //문자 'HELLO'는 짝수인가 홀수인가
 console.log(checkStringOddOrEven('HELLO'))
+
+//결과-> 터미널에서 확인
+PS C:\Users\User\Desktop\백엔드\백엔드js> node index
+짝수
+홀수
